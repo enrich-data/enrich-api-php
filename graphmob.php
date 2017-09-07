9a683b6f531b9cabb5844ff329479b532a590c8c
Valerian Saliou <valerian@valeriansaliou.name>
2017-09-07 18:02:50 +0200

    v1.0.0
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- graphmob.php
+++ graphmob.php
@@ -12,19 +12,25 @@ require __DIR__."/resources/Search.php";
 require __DIR__."/resources/Verify.php";
 
 class Graphmob {
-  public $DEFAULT_REST_HOST = "https://api.graphmob.com";
-  public $DEFAULT_REST_BASE_PATH = "/v1";
+  private $DEFAULT_REST_HOST = "https://api.graphmob.com";
+  private $DEFAULT_REST_BASE = "/v1";
+  private $DEFAULT_TIMEOUT = 5000;
+
+  private $CREATED_STATUS_CODE = 201;
+  private $NOT_FOUND_STATUS_CODE = 404;
+  private $CREATED_RETRY_COUNT_MAX = 2;
 
   public function __construct() {
     $this->auth = [];
 
     $this->_rest = new RestClient([
       "user_agent"   => "graphmob-api-php/1.0.0",
-      "base_url"     => $this->DEFAULT_REST_HOST.$this->DEFAULT_REST_BASE_PATH,
+      "base_url"     => $this->DEFAULT_REST_HOST.$this->DEFAULT_REST_BASE,
       "content_type" => "application/json",
 
       "curl_options" => [
-        CURLOPT_TIMEOUT => 5000,
+        CURLOPT_CONNECTTIMEOUT => $DEFAULT_TIMEOUT,
+        CURLOPT_TIMEOUT => $DEFAULT_TIMEOUT,
         CURLOPT_SSL_VERIFYHOST => 2
       ],
 
@@ -42,10 +48,6 @@ class Graphmob {
     $this->verify = new GraphmobVerify($this);
   }
 
-  public function _get($resource, $query) {
-    return $this->_rest->get($resource, $query)->decode_response();
-  }
-
   public function setRestHost($host) {
     $this->_rest->set_option("base_url", $host);
   }
@@ -53,6 +55,44 @@ class Graphmob {
   public function authenticate($identifier, $key) {
     $this->_rest->set_option("username", $identifier);
     $this->_rest->set_option("password", $key);
+  }
+
+  public function _get($resource, $query) {
+    return $this->_doGet($resource, $query, 0, 0);
+  }
+
+  private function _doGet($resource, $query, $retryCount, $holdForSeconds) {
+    // Abort?
+    if ($retryCount > $this->CREATED_RETRY_COUNT_MAX) {
+      return [
+        "error" => [
+          "reason" => "not_found",
+          "message" => (
+            "The requested item was not found, after attempted discovery."
+          )
+        ]
+      ];
+    } else {
+      // Hold.
+      sleep($holdForSeconds);
+
+      $response = $this->_rest->get($resource, $query);
+
+      // Re-schedule request? (created)
+      if ($response->info->http_code === $this->CREATED_STATUS_CODE ||
+          ($retryCount > 0 && $response->info->http_code ===
+            $this->NOT_FOUND_STATUS_CODE)) {
+        if ($response->headers->retry_after) {
+          $holdForSeconds = intval($response->headers->retry_after);
+        }
+
+        return $this->_doGet(
+          $resource, $query, $retryCount + 1, $holdForSeconds
+        );
+      }
+
+      return $response->decode_response();
+    }
   }
 }
 
