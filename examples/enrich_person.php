d2564ee9659a84c86a9ae5a53b0e1e929ce313e0
Valerian Saliou <valerian@valeriansaliou.name>
2017-09-09 14:38:55 +0200

    Fix max line len
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- examples/enrich_person.php
+++ examples/enrich_person.php
@@ -11,7 +11,10 @@ require __DIR__."/../vendor/autoload.php";
 
 $client = new Graphmob();
 
-$client->authenticate("ui_a311da78-6b89-459c-8028-b331efab20d5", "sk_f293d44f-675d-4cb1-9c78-52b8a9af0df2");
+$client->authenticate(
+  "ui_a311da78-6b89-459c-8028-b331efab20d5",
+  "sk_f293d44f-675d-4cb1-9c78-52b8a9af0df2"
+);
 
 $data = $client->enrich->person([
   "email" => "valerian@crisp.chat"
