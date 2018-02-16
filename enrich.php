055228add16a6d7e00f5038fdee547efcd044d5e
Valerian Saliou <valerian@valeriansaliou.name>
2018-02-16 22:01:41 +0100

    v1.2.0
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- enrich.php
+++ enrich.php
@@ -13,17 +13,13 @@ require __DIR__."/resources/Verify.php";
 class Enrich {
   private $DEFAULT_REST_HOST = "https://api.enrichdata.com";
   private $DEFAULT_REST_BASE = "/v1";
-  private $DEFAULT_TIMEOUT = 5000;
-
-  private $CREATED_STATUS_CODE = 201;
-  private $NOT_FOUND_STATUS_CODE = 404;
-  private $CREATED_RETRY_COUNT_MAX = 10;
+  private $DEFAULT_TIMEOUT = 40000;
 
   public function __construct() {
     $this->auth = [];
 
     $this->_rest = new RestClient([
-      "user_agent"   => "enrich-api-php/1.1.4",
+      "user_agent"   => "enrich-api-php/1.2.0",
       "base_url"     => $this->DEFAULT_REST_HOST.$this->DEFAULT_REST_BASE,
       "content_type" => "application/json",
 
@@ -56,41 +52,11 @@ class Enrich {
   }
 
   public function _get($resource, $query) {
-    return $this->_doGet($resource, $query, 0, 0);
+    return $this->_doGet($resource, $query);
   }
 
-  private function _doGet($resource, $query, $retryCount, $holdForSeconds) {
-    // Abort?
-    if ($retryCount > $this->CREATED_RETRY_COUNT_MAX) {
-      return [
-        "error" => [
-          "reason" => "not_found",
-          "message" => (
-            "The requested item was not found, after attempted discovery."
-          )
-        ]
-      ];
-    } else {
-      // Hold.
-      sleep($holdForSeconds);
-
-      $response = $this->_rest->get($resource, $query);
-
-      // Re-schedule request? (created)
-      if ($response->info->http_code === $this->CREATED_STATUS_CODE ||
-          ($retryCount > 0 && $response->info->http_code ===
-            $this->NOT_FOUND_STATUS_CODE)) {
-        if ($response->headers->retry_after) {
-          $holdForSeconds = intval($response->headers->retry_after);
-        }
-
-        return $this->_doGet(
-          $resource, $query, $retryCount + 1, $holdForSeconds
-        );
-      }
-
-      return $response->decode_response();
-    }
+  private function _doGet($resource, $query) {
+    return $this->_rest->get($resource, $query)->decode_response();
   }
 }
 
